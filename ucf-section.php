eaccd5335d1afd88d93d66ae93549def4b0c19e7
Cadie Stockman <cadie.brown@gmail.com>
2020-06-19 10:31:24 -0400

    Bump version number

--- ucf-section.php
+++ ucf-section.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: UCF Section
 Description: Provides a shortcode, functions, and default styles for displaying Sections.
-Version: 1.1.0
+Version: 1.1.1
 Author: UCF Web Communications
 License: GPL3
 */
